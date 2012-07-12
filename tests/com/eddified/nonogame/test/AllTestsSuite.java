f5cb2b04ea5372955155280e62fa270f7566cc89
Eddie Bishop <eddie.bishop@gmail.com>
2012-07-11 23:32:46 -0700

    add dummy tests

---
+++ tests/com/eddified/nonogame/test/AllTestsSuite.java
@@ -0,0 +1,11 @@
+package com.eddified.nonogame.test;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ AllTestsTest.class })
+public class AllTestsSuite {
+
+}
---
+++ tests/com/eddified/nonogame/test/AllTestsTest.java
@@ -0,0 +1,14 @@
+package com.eddified.nonogame.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class AllTestsTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
